bfdba4ff059c408e229e5d478c77c91ad4d14194
Gouravdeep Singh <gouravdhaliwal88@gmail.com>
2025-03-31 18:52:35 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gouravdeep Singh on 2025-03-31.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
